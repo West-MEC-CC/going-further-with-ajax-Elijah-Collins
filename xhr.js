c7d07c392a58ed9377bf5513644a3fba5eca6ac6
Koro Sensei <51177688+khadeembernard@users.noreply.github.com>
2020-10-14 15:05:14 -0700

    Initial commit

---
+++ xhr.js
@@ -0,0 +1,17 @@
+////////////////Use Dom to select get and post button/////////////
+
+///////create callback function thats used when button clicked////////
+
+////////// Create callback function with embeded promise/////////////
+
+///////////// When browser load parse http req to usable format//////////
+
+//////// Assign diferent parts to pokedex using dom selection/////////
+
+
+/// add event listener to get button ///
+
+
+
+
+
